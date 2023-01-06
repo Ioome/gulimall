1790a5adc78c2df3dd5c72054441f4c450c3d973
sutton <50450947+Ioome@users.noreply.github.com>
2023-01-06 15:31:22 +0800

    sutton

---
+++ gulimall-common/src/main/java/com/atguigu/common/to/SkuHasStockTo.java
@@ -0,0 +1,13 @@
+package com.atguigu.common.to;
+
+import lombok.Data;
+
+@Data
+public class SkuHasStockTo {
+    private Long skuId;
+    private boolean hasStock;
+
+    public boolean getHasStock(){
+        return this.hasStock;
+    }
+}
